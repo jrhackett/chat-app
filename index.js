22cf6fa2c70342c6e382c3a5a0a85acc40f06bad
Jacob Hackett <jacobrhackett@gmail.com>
2017-04-28 15:33:19 -0400

    adding max message sizes

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('chat message', msg.substring(0, 1000));
   });
 });
 
